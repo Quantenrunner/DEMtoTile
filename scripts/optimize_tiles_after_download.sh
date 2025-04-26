017b678e51ea9e12b1b12ce1e85fc8d27c410486
Quantenrunner <hun_2025@eclipso.ch>
2025-04-26 02:36:06 +0200

    fix optimize_tiles_after_download.sh

--- scripts/optimize_tiles_after_download.sh
+++ scripts/optimize_tiles_after_download.sh
@@ -28,7 +28,8 @@ du -sh "$TILES_DIR"
 
 process_file() {
     FILE="$1"
-        if [[ "$(magick identify -format "%[opaque]" "$FILE")" == "False" ]]; then
+#        if [[ "$(magick identify -format "%[opaque]" "$FILE")" == "False" ]]; then
+        if [[ "$(convert "$FILE" -format "%[opaque]" info:)" = "False" ]]; then
             #magick "$FILE" -background white -alpha remove -alpha off "$FILE"
             convert "$FILE" -background white -alpha remove -alpha off "$FILE"
         fi
